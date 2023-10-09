e8093406adb1cd0f66e4c72a6b1ae94f9bbbc328
ivan klimentev <klimentievis@inbox.ru>
2023-10-09 07:49:50 +0300

    wrk

--- menu/templatetags/tags.py
+++ menu/templatetags/tags.py
@@ -12,10 +12,13 @@ def draw_menu(context, name):
     items = MenuItem.objects.filter(menu__name=name).select_related('parent').all()
     menu_names = context['request'].GET
     active_node = menu_names.get(name)
-    items_list = build_tree(items)
-    pp.pprint(items_list)
-    search_item(items_list, active_node)
-    return {'items': items_list, 'name': name}
+    if not active_node:
+        items_dict = {'name': name, 'submenus': []}
+        return {'items': items_dict, 'name': name}
+    items_dict = {'name': name, 'submenus': build_tree(items)}
+    # pp.pprint(items_dict)
+    pp.pprint(depth_first_search(items_dict, active_node)[0])
+    return {'items': items_dict, 'name': name}
 
 def build_tree(items, parent=None):
     res = []
@@ -24,12 +27,25 @@ def build_tree(items, parent=None):
             res.append({'name': item.name, 'submenus': build_tree(items, item)})
     return res
 
-def search_item(tree, name, found=False):
-    for node in tree:
-        print(node['name'])
-        if node['name'] == name:
-            found = True
-            return found
-        if 'submenus' in node and not found:
-            found = search_item(node['submenus'], name, found)
-            return found
+
+def depth_first_search(structure, stop_name):
+    current_name = structure['name']
+    res = {'name': current_name, 'submenus': []}
+
+    if current_name == stop_name:
+        if structure['submenus']:
+            for submenu in structure['submenus']:
+                res['submenus'].append({'name': submenu['name'], 'submenus': []})
+            return (res, True)
+        return (res, True)
+
+    if not structure['submenus']:
+        return (res, False)
+    
+    for submenu in structure['submenus']:
+        result, found = depth_first_search(submenu, stop_name)
+        res['submenus'].append(result)
+        if found:
+            return (res, True)
+
+    return (res, False)
